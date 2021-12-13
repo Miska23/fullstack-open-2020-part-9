439268fe48d5b4afbf772d47d253406bfb4b0d21
Miska23 <miskalinden@gmail.com>
2021-12-13 19:19:55 +0200

    start exercise 9.5

--- bmiCalculator.ts
+++ bmiCalculator.ts
@@ -1,25 +1,25 @@
 type BmiResult = 'Underweight' | 'Normal' | 'Overweight'
 
-interface BmiInput {
+export interface BmiInput {
   heightInCm: number;
   weight: number;
 }
 
-const parseBmiArguments = (args: Array<string>): BmiInput => {
-  if (args.length < 4) throw new Error('Not enough arguments');
-  if (args.length > 4) throw new Error('Too many arguments');
+// const parseBmiArguments = (args: Array<string>): BmiInput => {
+//   if (args.length < 4) throw new Error('Not enough arguments');
+//   if (args.length > 4) throw new Error('Too many arguments');
 
-  if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
-    return {
-      heightInCm: Number(args[2]),
-      weight: Number(args[3])
-    }
-  } else {
-    throw new Error('Provided values were not numbers!');
-  }
-}
+//   if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+//     return {
+//       heightInCm: Number(args[2]),
+//       weight: Number(args[3])
+//     }
+//   } else {
+//     throw new Error('Provided values were not numbers!');
+//   }
+// }
 
-const calculateBmi = (heightInCm: number, weight: number): void => {
+export const calculateBmi = (heightInCm: number, weight: number): BmiResult => {
   const heightInM = heightInCm / 100
   const bmi = Number((weight / Math.pow(heightInM, 2)).toFixed(1))
 
@@ -31,16 +31,16 @@ const calculateBmi = (heightInCm: number, weight: number): void => {
   } else {
     resultText = 'Overweight'
   }
- console.log(resultText)
+ return resultText
 }
 
-try {
-  const {heightInCm, weight} = parseBmiArguments(process.argv)
-  calculateBmi(heightInCm, weight)
-} catch (error: unknown) {
-  let errorMessage = 'Something bad happened.'
-  if (error instanceof Error) {
-    errorMessage += ' Error: ' + error.message;
-  }
-  console.log(errorMessage);
-}
+// try {
+//   const {heightInCm, weight} = parseBmiArguments(process.argv)
+//   calculateBmi(heightInCm, weight)
+// } catch (error: unknown) {
+//   let errorMessage = 'Something bad happened.'
+//   if (error instanceof Error) {
+//     errorMessage += ' Error: ' + error.message;
+//   }
+//   console.log(errorMessage);
+// }
--- index.ts
+++ index.ts
@@ -1,8 +1,51 @@
 import express from 'express';
+import { BmiInput, calculateBmi } from './bmiCalculator';
+
+
 const app = express();
+
+interface BmiQuery {
+  height: string,
+  weight: string,
+}
+
+const isBmiQuery = (value: any): value is BmiQuery => {
+  const {height, weight} = value
+  return height !== undefined && weight !== undefined
+}
+
+const parseQuery = (query: any): BmiInput =>  {
+  if (isBmiQuery(query)) {
+    if (!isNaN(Number(query.height)) && !isNaN(Number(query.weight))) {
+      return {
+        heightInCm: Number(query.height),
+        weight: Number(query.weight),
+      }
+    } else {
+      throw new Error('Query strings were not numbers!');
+    }
+  } else {
+    throw new Error('Invalid query string format!');
+  }
+}
 
 app.get('/hello', (_req, res) => {
   res.send('Hello Full Stack');
+});
+
+app.get('/bmi', (req, res) => {
+  try {
+    const {heightInCm, weight} = parseQuery(req.query)
+    console.log("parseQuery(req.query): ", parseQuery(req.query));
+
+    res.json({bmi: calculateBmi(heightInCm, weight)})
+  } catch (error) {
+    let errorMessage = 'Something went wrong.';
+    if (error instanceof Error) {
+      errorMessage += ' Error: ' + error.message;
+    }
+    res.status(400).send(errorMessage);
+  }
 });
 
 const PORT = 3003;
